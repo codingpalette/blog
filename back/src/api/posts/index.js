09219c45746a448de93e4de25eb23c0f21d8dd4b
이성재 <iseongjae@iseongjaeui-MacBookAir.local>
2020-01-04 18:01:50 +0900

    페이지 네이션 구현

--- back/src/api/posts/index.js
+++ back/src/api/posts/index.js
@@ -12,9 +12,18 @@ const posts = new Router();
 
 posts.get('/' , postsCtrl.list);
 posts.post('/' , postsCtrl.write);
-posts.get('/:id' , postsCtrl.read);
-posts.delete('/:id' , postsCtrl.remove);
-posts.patch('/:id' , postsCtrl.update);
+posts.get('/:id' , postsCtrl.checkObjectId , postsCtrl.read);
+posts.delete('/:id' , postsCtrl.checkObjectId , postsCtrl.remove);
+posts.patch('/:id' , postsCtrl.checkObjectId , postsCtrl.update);
+
+
+// const post = new Router(); // /api/posts/:id
+
+// post.get('/:id' , postsCtrl.read);
+// post.delete('/:id' , postsCtrl.remove);
+// post.patch('/:id' , postsCtrl.update);
+
+// posts.use('/:id' , postsCtrl.checkObjectId, post.routes());
 
 
 export default posts;
