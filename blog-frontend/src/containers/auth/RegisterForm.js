e060b6508e341a1c727218e17e98718b37e3b609
이성재 <iseongjae@iseongjaeui-MacBookAir.local>
2020-01-14 22:10:06 +0900

    회원가입 구현

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
-import { initializeForm, changeField } from '../../modules/auth';
+import { initializeForm, changeField, register } from '../../modules/auth';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
-    form: auth.register
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user
   }));
   // 인풋 변경 인벤트 핸들러
   const onChange = e => {
@@ -22,7 +27,12 @@ const RegisterForm = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-    // 구현 예정
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      // TODO : 오류처리
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   // 컴포넌트가 처음 렌더링될 때 formd을 초기화함
@@ -30,16 +40,32 @@ const RegisterForm = () => {
     dispatch(initializeForm('register'));
   }, [dispatch]);
 
+  // 회원가입 성공/실패 처리
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  // user 값이 잘 설정되었는지 확인
+  useEffect(() => {
+    if (user) {
+      history.push('/'); // 홈 화면으로 이동
+    }
+  }, [user, history]);
+
   return (
     <>
-      <AuthForm
-        type="register"
-        form={form}
-        onChange={onChange}
-        onSubmit={onSubmit}
-      />
+      <AuthForm type="register" form={form} onChange={onChange} onSubmit={onSubmit} />
     </>
   );
 };
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
