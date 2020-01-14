96debb026a871587405d1b90544d7e11c9380916
이성재 <iseongjae@iseongjaeui-MacBookAir.local>
2020-01-14 23:14:50 +0900

    header 컴포넌트 제작

--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 const WritePage = () => {
-    return(
-        <div>
-            <Button>버튼</Button>
-        </div>
-
-    )
+  return (
+    <div>
+      <Header />
+      <Button>버튼</Button>
+    </div>
+  );
 };
-
 
 export default WritePage;
