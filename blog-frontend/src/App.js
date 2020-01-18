c861a4bec4aa58e87f69ea8a1ec6563c09ad127b
이성재 <adfffewr@naver.com>
2020-01-18 20:54:45 +0900

    글쓰기 API 연동

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -4,18 +4,18 @@ import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
-import PostPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
 function App() {
-    return (
-        <>
-            <Route component={PostListPage} path={[ '/@username' , '/' ]} exact />
-            <Route component={LoginPage} path="/login" />
-            <Route component={RegisterPage} path="/register" />
-            <Route component={WritePage} path="/write" />
-            <Route component={PostPage} path= "/@username/:postId"  />
-        </>
-    );
+  return (
+    <>
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
+  );
 }
 
 export default App;
